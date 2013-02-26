972eb4b6888450ca3cff726b0b3b99c126294714
Alan Languirand <alan@13protons.com>
2013-02-26 16:32:56 -0500

    fist pass at building API craft conf website

---
+++ public/js/functions.js
@@ -0,0 +1,39 @@
+
+$(function(){
+	/*
+	$('#page').masonry({
+	    // options
+	    itemSelector : '.section',
+		columnWidth: function( containerWidth ) {
+		    return containerWidth / 2.1;
+		  }
+	});
+	*/
+	
+	// create a map in the "map" div, set the view to a given place and zoom
+	var map = L.map('map').setView([42.336076,-83.049292], 17);
+
+	/*
+	// add Cloudmade tile layer (better vis)
+	L.tileLayer('http://{s}.tile.cloudmade.com/5278d1bee38f4ebabc822e8d5a6faa2e/997/256/{z}/{x}/{y}.png', {
+	    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://cloudmade.com">CloudMade</a>',
+	    maxZoom: 18
+	}).addTo(map);
+	*/
+	
+	// add an OpenStreetMap tile layer
+	L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+	    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+	}).addTo(map);
+
+	// add a marker in the given location, attach some popup content to it and open the popup
+	L.marker([42.336076,-83.049292]).addTo(map).bindPopup('<h1>apicraftconf:</h1> M@dison Building<br/>1555 Broadway St')
+	    .openPopup();	
+	
+	
+		
+});
+
+
+
+
--- server.js
+++ server.js
@@ -1,9 +1,16 @@
 var express = require('express');
+var lessMiddleware = require('less-middleware');
 
 var app = express();
 app.engine('.html', require('ejs').__express);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'html');
+
+app.use(lessMiddleware({
+    src      : __dirname + "/public",
+    compress : true
+  }));
+app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(req, res) {
   res.render('index');
