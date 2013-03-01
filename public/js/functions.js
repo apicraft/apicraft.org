4604766fbd6a4f37c002ec7b49b1d8ce877e7ccb
Alan Languirand <alan@13protons.com>
2013-03-01 16:20:08 -0500

    added mapping functions

--- public/js/functions.js
+++ public/js/functions.js
@@ -1,44 +1,20 @@
 
 $(function(){
 	
-	var example = [{ 
-			id: 'the-westin-book-cadillac-detroit-detroit',
-			is_closed: false,
-			name: 'The Westin Book Cadillac Detroit',
-			image_url: 'http://s3-media1.ak.yelpcdn.com/bphoto/zKs2B4ohmZwhpraQ1PnbmA/ms.jpg',
-			display_phone: '+1-313-442-1600',
-			rating: 4,
-			rating_img_url_small: 'http://s3-media4.ak.yelpcdn.com/assets/2/www/img/f62a5be2f902/ico/stars/v1/stars_small_4.png',
-			location: 
-			 { city: 'Detroit',
-			   display_address: 
-			    [ '1114 Washington Blvd',
-			      'Downtown Detroit',
-			      'Detroit, MI 48226' ],
-			   geo_accuracy: 8,
-			   neighborhoods: [ 'Downtown Detroit' ],
-			   postal_code: '48226',
-			   country_code: 'US',
-			   address: [ '1114 Washington Blvd' ],
-			   coordinate: { latitude: 42.3317983, longitude: -83.0508191 },
-			   state_code: 'MI' 
-			}
-		}
-		] 
-	
-	console.log(example);
-	/*
-	$('#page').masonry({
-	    // options
-	    itemSelector : '.section',
-		columnWidth: function( containerWidth ) {
-		    return containerWidth / 2.1;
-		  }
+	var hotelIcon = L.icon({
+	    iconUrl: 'images/hotel_icon.png',
+	    iconRetinaUrl: 'images/hotel_icon@2x.png',
+	    iconSize: [40, 48],
+	    iconAnchor: [20, 48],
+	    popupAnchor: [0, -40],
+	    shadowUrl: 'images/hotel_icon_shadow.png',
+	    shadowRetinaUrl: 'images/hotel_icon_shadow@2x.png',
+	    shadowSize: [53, 46],
+	    shadowAnchor: [24, 48]
 	});
-	*/
 	
 	// create a map in the "map" div, set the view to a given place and zoom
-	var map = L.map('map', {"scrollWheelZoom": false}).setView([42.336076,-83.049292], 17);
+	var map = L.map('map', {"scrollWheelZoom": false}).setView([42.335000,-83.049292], 15);
 
 	/*
 	// add Cloudmade tile layer (better vis)
@@ -53,13 +29,48 @@ $(function(){
 	    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
 	}).addTo(map);
 	
-	$.each(example, function(i, v){
-		console.log(v);
-	})
+	$.ajax({
+		url: window.config.proxyUrl + "/locations/detroit/", 
+		crossDomain: true,
+		success: function(d){ 
+			console.log(d);
+			$.each(d, function(i, v){
+				if(!v.is_closed){
+					var options = null;
+					//build up the html
+					v.popupTitle = v.name
+					if(typeof(v.apicraftTitle) != "undefined"){v.popupTitle = v.apicraftTitle;}
+					
+					var html ='<h4><a href="'+v.url+'" title="Click to view on yelp">'+v.popupTitle+'</a></h4>'; //popup title
+					html +='<table><tr><td>';	//using a table
+					html +='<Strong>'+v.name+'</strong><br/>'; //name
+					html +='<p>Address</p>'; //address
+					
+					html +='</td><td>'; //cell #2
+					
+					html +='<img src="" alt="venue image" /><br />'; //venue image
+					html +='<img src="'+v.rating_img_url_small+'" alt="/5 Stars" /><br />'; //yelp rating
+					html +='<span class="phone">Phone Number</span>';//phone
+					
+					html +='</td></tr></table>'; //end of the table
+					
+					if(typeof(v.apicraftType) != "undefined" && v.apicraftType == "hotel") {options = {icon: hotelIcon};}
+					
+					//plot it on the map
+					// add a marker in the given location, attach some popup content to it and open the popup
+					var a = L.marker([v.location.coordinate.latitude, v.location.coordinate.longitude], options)
+						.addTo(map)
+						.bindPopup(html);
+					if(typeof(v.apicraftType) != "undefined" && v.apicraftType == "main_venue"){a.openPopup();}
+						
+				}
+			}); 
+		}
+	});
+	/*
 	
-	// add a marker in the given location, attach some popup content to it and open the popup
-	L.marker([42.336076,-83.049292]).addTo(map).bindPopup('<h4>apicraftconf</h4> M@dison Building - 1555 Broadway St')
-	    .openPopup();	
+	*/
+		
 	
 	
 		
