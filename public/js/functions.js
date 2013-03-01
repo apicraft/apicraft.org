fc8d8ddb18807c5deacfa377b9a5f2e918aca178
Alan Languirand <alan@13protons.com>
2013-03-01 12:38:23 -0500

    removed yelp info from server.js

--- public/js/functions.js
+++ public/js/functions.js
@@ -1,5 +1,32 @@
 
 $(function(){
+	
+	var example = [{ 
+			id: 'the-westin-book-cadillac-detroit-detroit',
+			is_closed: false,
+			name: 'The Westin Book Cadillac Detroit',
+			image_url: 'http://s3-media1.ak.yelpcdn.com/bphoto/zKs2B4ohmZwhpraQ1PnbmA/ms.jpg',
+			display_phone: '+1-313-442-1600',
+			rating: 4,
+			rating_img_url_small: 'http://s3-media4.ak.yelpcdn.com/assets/2/www/img/f62a5be2f902/ico/stars/v1/stars_small_4.png',
+			location: 
+			 { city: 'Detroit',
+			   display_address: 
+			    [ '1114 Washington Blvd',
+			      'Downtown Detroit',
+			      'Detroit, MI 48226' ],
+			   geo_accuracy: 8,
+			   neighborhoods: [ 'Downtown Detroit' ],
+			   postal_code: '48226',
+			   country_code: 'US',
+			   address: [ '1114 Washington Blvd' ],
+			   coordinate: { latitude: 42.3317983, longitude: -83.0508191 },
+			   state_code: 'MI' 
+			}
+		}
+		] 
+	
+	console.log(example);
 	/*
 	$('#page').masonry({
 	    // options
@@ -25,7 +52,11 @@ $(function(){
 	L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
 	    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
 	}).addTo(map);
-
+	
+	$.each(example, function(i, v){
+		console.log(v);
+	})
+	
 	// add a marker in the given location, attach some popup content to it and open the popup
 	L.marker([42.336076,-83.049292]).addTo(map).bindPopup('<h4>apicraftconf</h4> M@dison Building - 1555 Broadway St')
 	    .openPopup();	
--- server.js
+++ server.js
@@ -1,21 +1,6 @@
 var express = require('express');
 var lessMiddleware = require('less-middleware');
 
-/* Yelp stuff!
-*******************
-var yelp = require("yelp").createClient({
-  consumer_key: "5SqpGb9Y-jno81OO-VdJiA", 
-  consumer_secret: "J2Xm2uuWMwU_UexGCfg_VC9wznY",
-  token: "BPHSv44BV-e2nolQyUs1U1cSjWLxfbca",
-  token_secret: "QyRys2UNApDANO8ILK85FqwOOgc"
-});
-
-// See http://www.yelp.com/developers/documentation/v2/business
-yelp.business("supino-pizzeria-detroit", function(error, data) {
-  console.log(data);
-});
-*/
-
 var app = express();
 app.engine('.html', require('ejs').__express);
 app.set('views', __dirname + '/views');
