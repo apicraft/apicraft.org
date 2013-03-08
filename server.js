e475880302d2daecf6e39060229e91c14a9cea35
Alan Languirand <alan@13protons.com>
2013-03-08 15:57:46 -0500

    API page is now all ejs template powered

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var lessMiddleware = require('less-middleware');
+var fs = require('fs');
 
 var config = require('./config');
 
@@ -14,19 +15,63 @@ app.use(lessMiddleware({
   }));
 app.use(express.static(__dirname + '/public'));
 
-app.get('/', function(req, res) {
-  res.render('index', { 
-	proxyUrl: config.proxyUrl,  
-	page: {
-		title: "API Craft Conference",
-		footer: {
-			devs: {
-				text: "Non-Developers",
-				link: "/nondev"
-			}
+
+var file = __dirname + '/views/api_model.json';
+ 
+fs.readFile(file, 'utf8', function (err, data) {
+  if (err) { console.log('Error: ' + err); return; }
+  apipage = JSON.parse(data);
+});
+
+var apipage = {
+	title: "API Craft Conference",
+	footer: {
+		devs: {
+			text: "Non-Developers",
+			link: "/nondev"
 		}
-	}
-	});
+	},
+	resources: [
+		{
+			title: "/conferences/{city}",
+			verbs: [
+				{
+					type: "get",
+					url: "/conferences",
+					description: "List Conferences"
+				}
+				]
+		},
+		{
+			title: "/goals",
+			verbs: [
+				{
+					type: "get",
+					url: "/goals",
+					description: "Goals and Philosiphy"
+				}
+				]
+		},
+		{
+			title: "/questions",
+			verbs: [
+				{
+					type: "post",
+					url: "/questions",
+					description: "Ask a question"
+				},
+				{
+					type: "get",
+					url: "/questions",
+					description: "Q's about the event"
+				}
+				]
+		}
+		]
+}
+
+app.get('/', function(req, res) {
+  res.render('index', { proxyUrl: config.proxyUrl, page: apipage });
 });
 
 app.get('/nondev', function(req, res) {
