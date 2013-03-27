6f71734e65452615c9e234cd822179ec240a303a
Alan Languirand <alan@13protons.com>
2013-03-27 16:12:05 -0400

    POSTs are working

--- public/js/functions.js
+++ public/js/functions.js
@@ -1,45 +1,32 @@
 $(function(){
-	
-	$.getJSON("http://api.apicraft.org/conferences/detroit/", function(response){
+	var baseURL = "http://api.apicraft.org";
+	var api_url = baseURL + "/conferences/detroit";
+	/*
+	$.getJSON(api_url, function(response){
 		//console.log(response);
 	});
+	*/
+	var spin_options = {lines:9,length:3,width:2,radius:4,corners:1,rotate:0,color:'#fff',speed:1.2,trail:35,shadow:false,hwaccel:false,className:'spinner',zIndex:2e9,top:'2px',left:'10'};
 	
+
 	$(".verb").hover(function(){ $(this).addClass("hover"); }, function(){ $(this).removeClass("hover"); });
 	
 	$(".resource .verb").click(function(){
 			if($(this).data('verb')=="get"){
 				var verb = $(this);
 				var target = verb.parent().find(".response");
+
 				if(verb.hasClass("active")){ 
 					verb.toggleClass('active');
 					target.slideUp("fast"); 
-					
 				}else{
 					
-
-					var baseURL = "http://api.apicraft.org";
 					var requestURL = baseURL + $(this).data("resource");
 					target.addClass("language-javascript");
 					
 					target.find(".request_url").html("GET " + requestURL);
 					target.find(".raw_response a").hide();
-					target.find(".raw_response").spin({
-					  lines: 9, // The number of lines to draw
-					  length: 3, // The length of each line
-					  width: 2, // The line thickness
-					  radius: 4, // The radius of the inner circle
-					  corners: 1, // Corner roundness (0..1)
-					  rotate: 0, // The rotation offset
-					  color: '#fff', // #rgb or #rrggbb
-					  speed: 1.2, // Rounds per second
-					  trail: 35, // Afterglow percentage
-					  shadow: false, // Whether to render a shadow
-					  hwaccel: false, // Whether to use hardware acceleration
-					  className: 'spinner', // The CSS class to assign to the spinner
-					  zIndex: 2e9, // The z-index (defaults to 2000000000)
-					  top: '2px', // Top position relative to parent in px
-					  left: '10' // Left position relative to parent in px
-					});
+					target.find(".raw_response").spin(spin_options);
 					target.slideDown();
 					
 					$.ajax({
@@ -49,13 +36,10 @@ $(function(){
 									target.find("code").text(html);
 									
 									target.find(".raw_response a").attr({"href": requestURL});
-									target.find(".raw_response a").show();
-									
-									
+									target.find(".raw_response a").show();	
 								},
 						error: function(){
-								target.find("code").text("Sorry, resource not available (404)");
-								
+								target.find("code").text("Sorry, resource not available (404)");	
 						},
 						complete: function(){
 							target.find(".raw_response").spin(false);
@@ -63,10 +47,74 @@ $(function(){
 							verb.toggleClass('active');
 						}	
 					});
-				
+					
+				}
+			} 
+			else if($(this).data('verb')=="post"){
+				var verb = $(this);
+				var request = verb.parent().find(".request");
+				var response = verb.parent().find(".response");
+				if(verb.hasClass("active")){ 
+					//hide
+					verb.toggleClass('active');
+					request.slideUp("fast"); response.slideUp("fast");
+				}else{
+					request.slideDown();
+					verb.toggleClass('active');
 				}
 			}
 	});
 	
+	$(".request_form").submit(function(e){
+		//try/catch stops errors from bypassing the "return false" statement at the end of this callback
+		try{
+			var $data = {}
+			$(this).find("input").each(function(i, v){
+				//build up a request
+				$data[$(v).attr("name")] = $(v).val();
+			});
+			var verb = $(".resource_" + $(this).data("resource-index") + " .verb_" + $(this).data("post-index"));
+			var target = $(".resource_" + $(this).data("resource-index") + " .response_" + $(this).data("post-index"));
+			
+			var requestURL = baseURL + verb.data("resource");
+			log(requestURL);
+			target.addClass("language-javascript");
+					
+			target.find(".request_url").html("POST " + requestURL);
+			target.find(".raw_response a").hide();
+			target.find(".raw_response").spin(spin_options);
+			target.slideDown();
+					
+			//make the request
+			$.ajax({
+				url: api_url+"/questions",
+				data: $data,
+				method: "POST", 
+				success: function(d){
+					//display the response
+					var html = JSON.stringify(d, undefined, 1);
+					target.find("code").text(html);
+					
+					target.find(".raw_response a").attr({"href": api_url+"/questions"});
+					target.find(".raw_response a").show();
+				},
+				error: function(e){ 
+					target.find("code").text("Sorry, resource not available (404)");
+				},
+				complete: function(){
+							target.find(".raw_response").spin(false);
+							Prism.highlightAll();
+							//verb.toggleClass('active');
+						}
+
+			});
+			
+		}
+		catch(e){log(e);}
+		//stop default form submission behavior
+		return false;
+	});
+
+	function log(x){console.log(x);} //silence is close at hand
 	//$(".resource .verb:first").click();
 });
